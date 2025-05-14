d98d65f8a4088c295023d729625b54e340ad49b8
Yerdy <kycj3621@gmail.com>
2025-05-14 18:31:39 +0900

    t

--- spec-trackr-app/src/App.js
+++ spec-trackr-app/src/App.js
@@ -24,30 +24,23 @@
 
 // export default App;
 
-// import React, { useState } from "react";
-// import ComSp from "./com_sp";      // Desktop-1
-// import SpCom from "./sp_com";    // Desktop-2
+import React, { useState } from "react";
+import ComSp from "./com_sp";      // Desktop-1
+import SpCom from "./sp_com";    // Desktop-2
 
-// function App() {
-//   const [screen, setScreen] = useState("DESKTOP1");
+function App() {
+  const [screen, setScreen] = useState("DESKTOP1");
 
-//   return (
-//     <div>
-//       {screen === "DESKTOP1" && (
-//         <ComSp onSpecTabClick={() => setScreen("DESKTOP2")} />
-//       )}
-//       {screen === "DESKTOP2" && (
-//         <SpCom onCompanyTabClick={() => setScreen("DESKTOP1")} />
-//       )}
-//     </div>
-//   );
-// }
-
-// export default App;
-
-const App = () => {
   return (
-    <h1>GitHub Pages로 리액트 프로젝트 배포</h1>
+    <div>
+      {screen === "DESKTOP1" && (
+        <ComSp onSpecTabClick={() => setScreen("DESKTOP2")} />
+      )}
+      {screen === "DESKTOP2" && (
+        <SpCom onCompanyTabClick={() => setScreen("DESKTOP1")} />
+      )}
+    </div>
   );
-};
+}
+
 export default App;
